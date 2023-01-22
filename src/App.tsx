05a1752a6f4fe3c86dfba511fa1a46879022a6b9
Farfama <farfama.hargaaya@gmail.com>
2023-01-22 17:00:18 +0900

    Update
    Steam tags

--- src/App.tsx
+++ src/App.tsx
@@ -66,6 +66,12 @@ function App() {
           <p className='stream-name'> Farfama <span> <svg type="color-fill-current" width="16px" height="16px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px" className="badge"><path fill-rule="evenodd" clip-rule="evenodd" d="M12.5 3.5L8 2L3.5 3.5L2 8L3.5 12.5L8 14L12.5 12.5L14 8L12.5 3.5ZM7.00008 11L11.5 6.5L10 5L7.00008 8L5.5 6.5L4 8L7.00008 11Z"></path></svg></span> </p>
           <p className='stream-title'> My beautiful partner burns food!</p>
           <p className='stream-type'> <a  href=""> Just Chatting </a> </p>
+          <div className='tag-container'>
+            <p className='tag'> Talking </p>
+            <p className='tag'> Fun </p>
+            <p className='tag'> English </p>
+            <p className='tag'> Reaction </p>
+          </div>
         </div>
         <div className='stats-container'>
         <p> <svg type="color-text-accessible-red" width="20px" height="20px" version="1.1" viewBox="0 0 20 20" x="0px" y="0px" className="audience-icon"><g><path fill-rule="evenodd" d="M5 7a5 5 0 116.192 4.857A2 2 0 0013 13h1a3 3 0 013 3v2h-2v-2a1 1 0 00-1-1h-1a3.99 3.99 0 01-3-1.354A3.99 3.99 0 017 15H6a1 1 0 00-1 1v2H3v-2a3 3 0 013-3h1a2 2 0 001.808-1.143A5.002 5.002 0 015 7zm5 3a3 3 0 110-6 3 3 0 010 6z" clip-rule="evenodd"></path></g></svg> </p> 
